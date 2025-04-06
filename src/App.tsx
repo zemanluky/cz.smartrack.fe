f07bcc84f2dbfd92be329533a989e4e7ea476f56
Michal Horazny <mikihore@gmail.com>
2025-04-06 19:12:22 +0200

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <Button variant="destructive">Button</Button>
+    </>
+  )
+}
+
+export default App
